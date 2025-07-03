29c0dc808193068d3759249a603236cdcc7d2194
AndersonCarlosT <andercarlost@hotmail.com>
2025-07-03 18:12:14 -0500

    Create zonas_predefinidas.py

---
+++ zonas_predefinidas.py
@@ -0,0 +1,5 @@
+zonas = {
+    "Zona 1": {"left": 50, "top": 50, "right": 300, "bottom": 300},
+    "Zona 2": {"left": 100, "top": 100, "right": 400, "bottom": 350},
+    "Zona 3": {"left": 10, "top": 10, "right": 200, "bottom": 200},
+}
